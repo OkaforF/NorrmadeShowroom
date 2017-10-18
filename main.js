bb3cce423c54cedaa69785f7e58c8611a7a5c4ad
OkaforF <fionaokafor@gmail.com>
2017-10-18 11:27:57 +0200

    wednesday, first push

---
+++ main.js
@@ -0,0 +1,21 @@
+let arrayOfImages = [
+    "imgs/sheeprear.png",
+    "imgs/woodoo.png",
+    "imgs/steward.png"
+]
+
+
+let rightIcon = document.querySelector("#right-next-icon");
+
+
+
+
+
+rightIcon.addEventListener("click", nextImageShow);
+
+function nextImageShow() {
+
+    console.log("right icon clicked");
+
+
+};
